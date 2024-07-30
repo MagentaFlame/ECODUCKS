d956fd609dfe44f24e0ae70b83ec1f8f3d711c87
Adalyn Shen <amslys31@gmail.com>
2024-07-30 12:44:41 -0400

    Initial Commit

---
+++ ECODUCKS/ECODUCKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECODUCKSApp.swift
+//  ECODUCKS
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ECODUCKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
